d6cb83f6b572ab9c1139b5b15292e4659ecdcd12
Ivan Baksheev <zpix-dev@list.ru>
2022-04-05 20:54:38 +0700

    added secure bounds

--- src/lib/crypto/cryptodata.py
+++ src/lib/crypto/cryptodata.py
@@ -4,6 +4,9 @@ from Crypto.Util.number import isPrime
 
 from src.lib.crypto.cryptoexception import CryptoException
 
+SECURE_P_BOUND = 2 ** 256
+SECURE_CD_BOUND = 2 ** 128
+
 
 @dataclass
 class CryptoData:
@@ -22,6 +25,10 @@ class CryptoData:
             raise CryptoException("2*k can't be greater than N")
         if len(self.strings.keys()) != self.N:
             raise CryptoException("Strings key count must be equal to N")
+        if self.p < SECURE_P_BOUND:
+            raise CryptoException(f"p is not secure (lower than {SECURE_P_BOUND})")
+        if self.c < SECURE_CD_BOUND or self.d < SECURE_CD_BOUND:
+            raise CryptoException(f"c and d are not secure (either lower than {SECURE_CD_BOUND})")
         if not isPrime(self.p):
             raise CryptoException("p is not prime")
         if not pow(self.c * self.d, self.p - 1) == 1:
